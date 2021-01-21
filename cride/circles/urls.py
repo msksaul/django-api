48b0ac54fa134f9e59ab74ccf63c1392c8fa9c13
msksaul <64514098+msksaul@users.noreply.github.com>
2021-01-21 01:40:48 -0600

    Circles Path

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -3,9 +3,15 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import circles as circle_views
+from .views import memberships as membership_views
 
 router = DefaultRouter()
 router.register(r'circles', circle_views.CircleViewSet, basename='circle')
+router.register(
+  r'circles/(?P<slug_name>[-a-zA-Z0-0_]+)/members',
+  membership_views.MembershipViewSet,
+  basename='membership'
+)
 
 urlpatterns = [
   path('', include(router.urls))
