73a8d8bd7922851d1f8984eb83cef4f6a659801d
msksaul <64514098+msksaul@users.noreply.github.com>
2021-01-20 23:03:41 -0600

    Token Permissions

---
+++ cride/circles/permissions/circles.py
@@ -0,0 +1,18 @@
+from rest_framework.permissions import BasePermission
+
+from cride.circles.models import Membership
+
+class IsCircleAdmin(BasePermission):
+
+  def has_object_permission(self, request, view, obj):
+
+    try:
+      Membership.objects.get(
+        user=request.user,
+        circle=obj,
+        is_admin=True,
+        is_active=True
+      )
+    except Membership.DoesNotExist:
+      return False
+    return True
--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -1,20 +1,30 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
+from cride.circles.permissions.circles import IsCircleAdmin
 
 from cride.circles.serializers import CircleModelSerializer
 
 from cride.circles.models import Circle, Membership
 
-class CircleViewSet(viewsets.ModelViewSet):
+class CircleViewSet(mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
 
   serializer_class = CircleModelSerializer
-  permission_classes = (IsAuthenticated,)
 
   def get_queryset(self):
     queryset = Circle.objects.all()
     if self.action == 'list':
       return queryset.filter(is_public=True)
     return queryset
+
+  def get_permissions(self):
+    permissions = [IsAuthenticated]
+    if self.action in ['update', 'partial_update']:
+      permissions.append(IsCircleAdmin)
+    return [permission() for permission in permissions]
 
   def perform_create(self, serializer):
     circle = serializer.save()
