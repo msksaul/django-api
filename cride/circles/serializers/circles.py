b9b8d6445dbef50c3a07df20a4c4f925330b334a
msksaul <64514098+msksaul@users.noreply.github.com>
2021-01-20 22:35:59 -0600

    Circle Limit Validate

--- cride/circles/serializers/circles.py
+++ cride/circles/serializers/circles.py
@@ -4,6 +4,13 @@ from cride.circles.models import Circle
 
 class CircleModelSerializer(serializers.ModelSerializer):
 
+  members_limit = serializers.IntegerField(
+    required=False,
+    min_value=10,
+    max_value=300,
+  )
+  is_limited = serializers.BooleanField(default=False)
+
   class Meta:
 
     model = Circle
@@ -13,3 +20,16 @@ class CircleModelSerializer(serializers.ModelSerializer):
       'rides_offered', 'is_public',
       'is_limited', 'members_limit'
     )
+    read_only_fields = (
+      'is_public',
+      'verified',
+      'rides_offered',
+      'rides_taken',
+    )
+  
+  def validate(self, data):
+    members_limit = data.get('members_limit', None)
+    is_limited = data.get('is_limited', False)
+    if is_limited ^ bool(members_limit):
+      raise serializers.ValidationError('If circle is limited, limit must be provided')
+    return data
--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from cride.circles.serializers import CircleModelSerializer
 
-from cride.circles.models import Circle
+from cride.circles.models import Circle, Membership
 
 class CircleViewSet(viewsets.ModelViewSet):
 
@@ -15,3 +15,15 @@ class CircleViewSet(viewsets.ModelViewSet):
     if self.action == 'list':
       return queryset.filter(is_public=True)
     return queryset
+
+  def perform_create(self, serializer):
+    circle = serializer.save()
+    user = self.request.user
+    profile = user.profile
+    Membership.objects.create(
+      user=user,
+      profile=profile,
+      circle=circle,
+      is_admin=True,
+      remaining_invitations=10
+    )
