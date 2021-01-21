87d421a208cd8ee4fdc779b55c59cced85f2cf5c
msksaul <64514098+msksaul@users.noreply.github.com>
2021-01-21 03:03:21 -0600

    Generate Invitations

--- cride/circles/views/memberships.py
+++ cride/circles/views/memberships.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets, mixins
 from rest_framework.generics import get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from cride.circles.models import Circle, Membership
+from cride.circles.models import Circle, Membership, Invitation
 
 from rest_framework.permissions import IsAuthenticated
 from cride.circles.permissions.memberships import IsActiveCircleMember
@@ -44,3 +46,36 @@ class MembershipViewSet(mixins.ListModelMixin,
   def perform_destroy(self, instance):
     instance.is_active = False
     instance.save()
+
+  @action(detail=True, methods=['get'])
+  def invitations(self, request, *args, **kwargs):
+
+    member = self.get_object()
+
+    invited_members = Membership.objects.filter(
+      circle=self.circle,
+      invited_by=request.user,
+      is_active=True
+    )
+
+    unused_invitations = Invitation.objects.filter(
+      circle=self.circle,
+      issued_by=request.user,
+      used=False
+    ).values_list('code')
+    diff = member.remaining_invitations - len(unused_invitations)
+
+    invitations = [x[0] for x in unused_invitations]
+    for i in range(0, diff):
+      invitations.append(
+        Invitation.objects.create(
+          issued_by=request.user,
+          circle=self.circle
+        ).code
+      )
+
+    data = {
+      'used_invitations': MembershipModelSerializer(invited_members, many=True).data,
+      'invitations': invitations
+    }
+    return Response(data)
